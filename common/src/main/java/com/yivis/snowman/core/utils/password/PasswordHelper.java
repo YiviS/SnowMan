f3f051bb8562ea1cb4789a0fc919e32db9be705d
Yivis <xuguang93@qq.com>
2017-02-16 20:08:58 +0800

    加密密码

---
+++ common/src/main/java/com/yivis/snowman/core/utils/password/PasswordHelper.java
@@ -0,0 +1,27 @@
+package com.yivis.snowman.core.utils.password;
+
+import com.yivis.snowman.sys.entity.SysUser;
+import org.apache.shiro.crypto.RandomNumberGenerator;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
+
+/**
+ * Created by XuGuang on 2017/2/16.
+ * 加密密码
+ */
+public class PasswordHelper {
+    private RandomNumberGenerator randomNumberGenerator = new SecureRandomNumberGenerator();
+    private String algorithmName = "md5";
+    private final int hashIterations = 2;
+
+    public void encryptPassword(SysUser user) {
+        user.setSalt(randomNumberGenerator.nextBytes().toHex());
+        String newPassword = new SimpleHash(
+                algorithmName,
+                user.getPassword(),
+                ByteSource.Util.bytes(user.getCredentialsSalt()),
+                hashIterations).toHex();
+        user.setPassword(newPassword);
+    }
+}
