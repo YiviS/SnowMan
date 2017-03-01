b8198736216ce8933aa8d87f105659771c979594
Yivis <xuguang93@qq.com>
2017-03-01 16:10:38 +0800

    系统变量配置

---
+++ snowman-web/src/main/java/com/yivis/snowman/core/config/SysConfig.java
@@ -0,0 +1,26 @@
+package com.yivis.snowman.core.config;
+
+/**
+ * Created by XuGuang on 2017/3/1.
+ * 项目属性
+ */
+public class SysConfig {
+
+    public static SysConfig sysConfig = new SysConfig();
+
+    /**
+     * 用户状态：online（在线） kickout（踢出）locked（锁定）
+     */
+    public final static String ONLINE_STATUS = "online_status";
+
+    public static enum OnlineStatus {
+        online("在线"),locked("锁定"), hidden("隐身"), kickout("踢出");
+        private final String info;
+        private OnlineStatus(String info) {
+            this.info = info;
+        }
+        public String getInfo() {
+            return info;
+        }
+    }
+}
