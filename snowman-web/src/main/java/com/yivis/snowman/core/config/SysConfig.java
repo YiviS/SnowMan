51d16bd837d6c03af407dee9d2a904a3031eac02
Yivis <xuguang93@qq.com>
2017-03-02 23:53:57 +0800

    全局变量

--- snowman-web/src/main/java/com/yivis/snowman/core/config/SysConfig.java
+++ snowman-web/src/main/java/com/yivis/snowman/core/config/SysConfig.java
@@ -9,16 +9,22 @@ public class SysConfig {
     public static SysConfig sysConfig = new SysConfig();
 
     /**
+     * 验证码开关 false（关） true（开）
+     */
+    public final static boolean CAPTCHA_SWITCH = false;
+    /**
      * 用户状态：online（在线） kickout（踢出）locked（锁定）
      */
     public final static String ONLINE_STATUS = "online_status";
 
     public static enum OnlineStatus {
-        online("在线"),locked("锁定"), hidden("隐身"), kickout("踢出");
+        online("在线"), locked("锁定"), hidden("隐身"), kickout("踢出");
         private final String info;
+
         private OnlineStatus(String info) {
             this.info = info;
         }
+
         public String getInfo() {
             return info;
         }
